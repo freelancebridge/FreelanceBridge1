6b2d9e75c245fcc62efd201b873beccc21367030
Dmitry <vibrastore.help@gmail.com>
2026-03-04 15:51:24 +0400

    force build

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    unoptimized: true,
-  }
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
+  images: { unoptimized: true },
+  // Добавляем эту строку, чтобы пропустить генерацию страниц с базой данных при сборке
+  output: 'standalone',
 };
 
 export default nextConfig;
