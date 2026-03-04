1485956be7215dada35f699b2ac6a33ff0971b72
Dmitry <vibrastore.help@gmail.com>
2026-03-04 15:56:11 +0400

    final bypass

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ const nextConfig = {
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
   images: { unoptimized: true },
-  // Добавляем эту строку, чтобы пропустить генерацию страниц с базой данных при сборке
-  output: 'standalone',
+  // Это отключит генерацию страниц, требующих базу данных, во время сборки
+  output: 'standalone'
 };
 
 export default nextConfig;
